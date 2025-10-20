cbc9bf8f7d852fedd3ef7068e8a83790d0b0534a
CarlosSamperFPGS <carsamene@alu.edu.gva.es>
2025-10-20 08:44:22 +0200

    Small annotiations

--- src/Tema2/Tema2Ejercicio24.java
+++ src/Tema2/Tema2Ejercicio24.java
@@ -6,7 +6,7 @@ public class Tema2Ejercicio24 {
     //Programa que permite a un profesor introducir la nota de un número indefinido de alumnos y le indica cúantos suspensos o aprobados hay.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double nota = 1;
+        double nota;
         //Si no inicializo la variable nota con un valor me da error (Consultar Ana) Pero ponerlo me indica un warning igualmente
         int aprobados = 0;
         int suspensos = 0;
